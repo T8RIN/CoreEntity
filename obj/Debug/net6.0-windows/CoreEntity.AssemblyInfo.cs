02a00decdaad8d7da7d79955b0871d64d3672933
T8RIN <oolbp@bk.ru>
2024-12-21 11:57:28 +0300

    -

--- obj/Debug/net6.0-windows/CoreEntity.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/CoreEntity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreEntity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544ba189f14f0e0254f213fc7bc7f5d4dc0edfc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreEntity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreEntity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
